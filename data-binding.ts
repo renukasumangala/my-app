36421329c50af635a666dccbbfae14b0235afc71
Renuka <renukasumangala.b@gmail.com>
2025-09-06 13:44:02 +0530

    Add files via upload
    Added data binding

--- data-binding.ts
+++ data-binding.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isReactive } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'app-data-binding',
@@ -7,13 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './data-binding.css'
 })
 export class DataBinding {
+  //string , number, boolean, date
 
   courseName: string  = "Angular 20";
+
+  isActive: boolean= false;
+  currentDate: Date = new Date();
+
+  rollNo: number = 222;
 
   constructor(){
     console.log(this.courseName);
 
     this.courseName = "Angular 20 Tutorial";
     console.log(this.courseName);
+
+    //this.courseName = "123";
   }
 }
