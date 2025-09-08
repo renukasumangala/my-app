b11c47bfebf74e0143bc4657233fdfb29a477570
Renuka <renukasumangala.b@gmail.com>
2025-09-08 14:27:46 +0530

    Add files via upload
    Added Angular  Control Flow

---
+++ control-flow.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-control-flow',
+  imports: [FormsModule],
+  templateUrl: './control-flow.html',
+  styleUrl: './control-flow.css'
+})
+export class ControlFlow {
+
+
+
+  selectedCaste: string = '';
+}
