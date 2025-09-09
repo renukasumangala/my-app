087b1872fbda0ada3f4e84b11fd53942a0069d36
Renuka <renukasumangala.b@gmail.com>
2025-09-09 13:18:53 +0530

    Add files via upload
    Added Angular Attribute Directive ( ngClass, ngStyle)

--- app.ts
+++ app.ts
@@ -1,18 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Admin } from './components/admin/admin';
 import { User } from './components/user/user';
 import { DataBinding } from './components/data-binding/data-binding';
 import { Signal } from './components/signal/signal';
 import { ControlFlow } from './components/control-flow/control-flow';
+import { Attribute } from './components/attribute/attribute';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Admin, User, DataBinding, Signal, ControlFlow],
+  imports: [RouterOutlet, Admin, User, DataBinding, Signal, ControlFlow, Attribute],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  protected readonly title = ('my-app');
 }
