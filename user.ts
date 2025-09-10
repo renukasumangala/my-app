62590d5efa55c76349f7419e11337d0ce8243a93
Renuka <renukasumangala.b@gmail.com>
2025-09-10 16:40:38 +0530

    Add files via upload
    Added user CRUD UI with reactive forms and API integration

--- user.ts
+++ user.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [ReactiveFormsModule,AsyncPipe],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
 export class User {
+
+
+  userForm: FormGroup = new FormGroup({
+    userId: new FormControl(0),
+    emailId: new FormControl(""),
+    password: new FormControl(""),
+    fullName: new FormControl(""),
+    mobileNo: new FormControl(""),
+  });
+
+  http = inject(HttpClient);
+ userList$: Observable<any[]>;
+
+ constructor() {
+  this.userList$ = this.http.get<any[]>("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers");
+ }
+
+ onClear() {
+  this.userForm.reset()
+ }
+ onEdit(data: any) {
+this.userForm = new FormGroup({
+    userId: new FormControl(data.userId),
+    emailId: new FormControl(data.emailId),
+    password: new FormControl(data.password),
+    fullName: new FormControl(data.fullName),
+    mobileNo: new FormControl(data.mobileNo),
+  });
+ }
+
+  onSaveUser() {
+    debugger;
+    const formValue = this.userForm.value;
+
+    this.http.post("http://api.freeprojectapi.com/api/GoalTracker/register", formValue).subscribe({
+      next: (response: any) => {
+        alert("User created success")
+      },
+      error: (error: any) => {
+        alert(error)
+      }
+    })
+
+  }
 
 }
